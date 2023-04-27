f4002f814b754c956fbff41178d8af884102e9af
HanHyunSoo <gustn4563@gmail.com>
2023-04-27 18:14:28 +0900

    feat: Axios Config

--- src/main.js
+++ src/main.js
@@ -2,9 +2,19 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import axios from 'axios';
 
 loadFonts()
 
-createApp(App)
-  .use(vuetify)
-  .mount('#app')
+axios.interceptors.request.use(
+  config => {
+    config.baseURL = 'https://port-0-swm-mini-project-41-back-17xqnr2algrz2xh8.sel3.cloudtype.app'
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
+
+const app = createApp(App)
+app.use(vuetify).mount('#app')
